2e7869f4d7165ff656754be9a196c6658c608442
Евгений <evgenij@MacBook-Air-Aleksej.local>
2023-04-22 06:27:15 -0700

    Выполнили домашнее задание

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿//Задача 41
+int Prompt (string message)
+{
+    System.Console.Write(message);
+    string value = Console.ReadLine();
+    int result = Convert.ToInt32(value);
+    return result;
+} 
+
+
+//Задача 43
+
+Console.WriteLine("Введите переменную b1: ");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите переменную k1: ");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите переменную b2: ");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите переменную k2: ");
+double k2 = Convert.ToDouble(Console.ReadLine());
+
+double x = -(b1 - b2) / (k1 - k2);
+double y = k1 * x + b1;
+
+Console.Write($" Точка пересечения двух прямых: [{x},{y}] ");
